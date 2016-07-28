55b598197ed1c6fe1c5a92faf9414b6a235b707e
Your Name <you@example.com>
2016-07-28 23:23:27 +0300

    Кое как я сделал это

--- blog/views.py
+++ blog/views.py
@@ -15,31 +15,33 @@ def post_detail(request, pk):
 
 
 def post_new(request):
+    form=PostForm()
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            if request.POST.get("publish_now"):
+                post.published_date = timezone.now()
             #post.published_date = timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
-        else:
-            form = PostForm()
-            return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request,'blog/post_edit.html',{'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    form=PostForm()
     if request.method == "POST":
         form = PostForm(request.POST,instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            if request.POST.get("publish_now"):
+                post.published_date = timezone.now()
             #post.published_date = timezone.now() - мгновенная публикация
             post.save()
             return render(request, 'blog/post_edit.html', {'form': form, 'pk': pk})
-        else:
-            form = PostForm(instance=post)
-            return render(request, 'blog/post_edit.html', {'form': form, 'pk': pk})
+    return render(request,'blog/post_edit.html',{'form': form,'pk': pk})
 
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
